7529d3e9fa6a8f425e34775fd4133b0c78d4df6b
deanteagleham <agleham27@up.edu>
2023-09-14 14:01:14 -0700

    Lab2

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -3,6 +3,8 @@ package cs301.birthdaycake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goodbye(View button){
+        Log.i("button", "Goodbye");
     }
 }
